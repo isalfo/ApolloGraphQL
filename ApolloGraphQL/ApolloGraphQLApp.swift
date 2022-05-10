2768f5318d252e63b4c9d2dbf4335f23df8a4d7c
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-05-10 11:54:09 -0300

    feat: add apollo, keychain and sdWebImage packages

---
+++ ApolloGraphQL/ApolloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGraphQLApp.swift
+//  ApolloGraphQL
+//
+//  Created by Gonzalo Alfonso on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
