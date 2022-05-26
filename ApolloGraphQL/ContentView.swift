0cca43fec4d60c79f37755af73e1180ecafa5dc8
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-05-26 10:41:58 -0300

    feat: API call and show the results on a list, graphqlError handled

--- ApolloGraphQL/ContentView.swift
+++ ApolloGraphQL/ContentView.swift
@@ -9,25 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
   @State var testText: String = ""
-    var body: some View {
-      NavigationView {
-          Text(testText)
-      }
-      .onAppear {
-        Network.shared.apollo.fetch(query: LaunchListQuery()) { result in
-          switch result {
-          case .success(let graphQLResult):
-            testText = "Success! Result: \(graphQLResult)"
-          case .failure(let error):
-            testText = "Failure! Error: \(error)"
+  @State private var launches = [LaunchListQuery.Data.Launch.Launch]()
+  @State private var showAlert: Bool = false
+  @State private var errorMessage: String = ""
+  
+  enum ListSection: Int, CaseIterable {
+    case launches
+  }
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(launches, id: \.id) { launch in
+          VStack {
+            Text(launch.site ?? "")
           }
         }
       }
     }
+    .alert(isPresented: $showAlert, content: {
+      Alert(title: Text("GraphQL Error"), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+    })
+    .onAppear {
+      Network.shared.apollo.fetch(query: LaunchListQuery()) { result in
+        switch result {
+        case .success(let graphQLResult):
+          if let launchConnection = graphQLResult.data?.launches {
+            self.launches.append(contentsOf: launchConnection.launches.compactMap { $0 })
+            if let errors = graphQLResult.errors {
+              let message = errors
+                    .map { $0.localizedDescription }
+                    .joined(separator: "\n")
+              errorMessage = message
+              showAlert = true
+              
+            }
+          }
+        case .failure(let error):
+          testText = "Failure! Error: \(error)"
+        }
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
